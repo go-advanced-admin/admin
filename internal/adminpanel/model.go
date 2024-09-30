4b9240d8cc76cd817629062b6b1123638bc43c63
Yidi Sprei <yidi.sprei@yidienterprises.com>
2024-09-30 13:52:28 -0400

    feat(adminpanel): introduce model-specific view links and handlers
    - Added `model.html` template for model-specific views.
    - Updated `app.html` and `root.html` to include links to model views.
    - Modified `app.go` to register routes for model view handlers.
    - Enhanced `model.go` with methods to generate model links and view handlers.
    - Improved navigation and user experience by allowing direct access to model views.

--- internal/adminpanel/model.go
+++ internal/adminpanel/model.go
@@ -1,5 +1,10 @@
 package adminpanel
 
+import (
+	"fmt"
+	"net/http"
+)
+
 type Model struct {
 	Name        string
 	DisplayName string
@@ -13,4 +18,35 @@ type AdminModelNameInterface interface {
 
 type AdminModelDisplayNameInterface interface {
 	AdminDisplayName() string
+}
+
+func (m *Model) GetLink() string {
+	return fmt.Sprintf("%s/%s", m.App.GetLink(), m.Name)
+}
+
+func (m *Model) GetFullLink() string {
+	return m.App.Panel.Config.GetLink(m.GetLink())
+}
+
+func (m *Model) GetViewHandler() HandlerFunc {
+	return func(data interface{}) (uint, string) {
+		allowed, err := m.App.Panel.PermissionChecker.HasModelReadPermission(m.App.Name, m.Name, data)
+		if err != nil {
+			return http.StatusInternalServerError, err.Error()
+		}
+		if !allowed {
+			return http.StatusForbidden, "Forbidden"
+		}
+
+		apps, err := GetAppsWithReadPermissions(m.App.Panel, data)
+		if err != nil {
+			return http.StatusInternalServerError, err.Error()
+		}
+
+		html, err := m.App.Panel.Config.Renderer.RenderTemplate("model.html", map[string]interface{}{"apps": apps, "model": m})
+		if err != nil {
+			return http.StatusInternalServerError, err.Error()
+		}
+		return http.StatusOK, html
+	}
 }
