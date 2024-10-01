310b4a3b6d121932cf69b1a39c8a4ab2532f77b0
Yidi <yidisprei@gmail.com>
2024-09-30 21:35:27 -0400

    chore: remove unused GetAssetsFile and GetTemplateFile functions
    These functions were redundant as they were not being used anywhere
    in the codebase. Removing them helps in cleaning up the code and
    improving maintainability.

--- internal/general_files.go
+++ internal/general_files.go
@@ -7,11 +7,3 @@ var AssetsFiles embed.FS
 
 //go:embed templates/*
 var TemplateFiles embed.FS
-
-func GetAssetsFile(fileName string) ([]byte, error) {
-	return AssetsFiles.ReadFile(fileName)
-}
-
-func GetTemplateFile(fileName string) ([]byte, error) {
-	return TemplateFiles.ReadFile(fileName)
-}
