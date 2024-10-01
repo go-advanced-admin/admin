42b112ae9c03a7b61ae0a8c84489dadda2018e91
Yidi Sprei <yidi.sprei@yidienterprises.com>
2024-10-01 14:25:12 -0400

    feat(tests): add FetchInstancesOnlyFieldWithSearch to MockORMIntegrator
    Added a new method FetchInstancesOnlyFieldWithSearch to the MockORMIntegrator
    in orm_mock_test.go. This enhances the mock's capability to support testing
    scenarios involving field-specific searches, improving test coverage and
    flexibility.

--- internal/adminpanel/orm_mock_test.go
+++ internal/adminpanel/orm_mock_test.go
@@ -9,3 +9,7 @@ func (m *MockORMIntegrator) FetchInstances(interface{}) (interface{}, error) {
 func (m *MockORMIntegrator) FetchInstancesOnlyFields(interface{}, []string) (interface{}, error) {
 	return []interface{}{}, nil
 }
+
+func (m *MockORMIntegrator) FetchInstancesOnlyFieldWithSearch(interface{}, []string, string, []string) (interface{}, error) {
+	return []interface{}{}, nil
+}
