1e522bbe8bae5ed3239f8477f93bee5fb6265612
Yidi Sprei <yidi.sprei@yidienterprises.com>
2024-10-02 15:48:49 -0400

    feat(form): add conversion function for form data to HTMLType map
    Introduced ConvertFormDataToHTMLTypeMap function to transform form
    data into a map with HTMLType values. Supports multiple values by
    encoding them as JSON, enhancing data handling flexibility.

--- internal/form/conversion.go
+++ internal/form/conversion.go
@@ -1,3 +1,23 @@
 package form
 
+import "encoding/json"
+
 type HTMLType string
+
+func ConvertFormDataToHTMLTypeMap(data map[string][]string) (map[string]HTMLType, error) {
+	result := make(map[string]HTMLType)
+
+	for key, values := range data {
+		if len(values) > 1 {
+			jsonEncoded, err := json.Marshal(values)
+			if err != nil {
+				return result, err
+			}
+			result[key] = HTMLType(jsonEncoded)
+		} else if len(values) == 1 {
+			result[key] = HTMLType(values[0])
+		}
+	}
+
+	return result, nil
+}
