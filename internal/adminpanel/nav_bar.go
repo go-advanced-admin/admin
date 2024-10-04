0557a0284dfa4b91e49a9b8e1572c37b8c0e78c8
nikki-sh <139382119+nikki-sh@users.noreply.github.com>
2024-10-03 20:24:12 -0400

    feat(adminpanel): fix rendering of navbar items

--- internal/adminpanel/nav_bar.go
+++ internal/adminpanel/nav_bar.go
@@ -3,17 +3,18 @@ package adminpanel
 import "fmt"
 
 type NavBarItem struct {
-	Name string
-	Link string
-	Bold bool
+	Name              string
+	Link              string
+	Bold              bool
+	NavBarAppendSlash bool
 }
 
 func (i *NavBarItem) HTML() string {
 	finalHTML := i.Name
 	if i.Bold {
-		finalHTML = `<h2 class="text-2x1 font-semibold m1-2 hidden lg:block">` + finalHTML + "</h2>"
+		finalHTML = `<h2 class="text-2x1 lg:block hidden font-semibold m1-2 pr-2">` + finalHTML + "</h2>"
 	}
-	if i.Link == "" {
+	if i.Link != "" {
 		finalHTML = fmt.Sprintf(`<a class="link" href="%s">%s</a>`, i.Link, finalHTML)
 	}
 	return finalHTML
